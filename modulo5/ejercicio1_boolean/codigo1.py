de537a08757330e7e9ec4d0c86e6a213e48232cd
TGabby-lab <ingrisduarte@mail.weber.edu>
2026-02-04 19:28:02 -0700

    Todo 1

--- modulo5/ejercicio1_boolean/codigo1.py
+++ modulo5/ejercicio1_boolean/codigo1.py
@@ -7,21 +7,35 @@ es_finde = False
 
 # TODO 1: Usa una expresión booleana con "and"
 # Por ejemplo: ¿Puede salir hoy solo si tiene 18 años o más Y si tiene permiso?
+edad = input("¿Cuántos años tienes? ")
+edad = int(edad)
+print("¿Puede salir solo si tiene 18 años o más y tiene permiso?")
+puede_salir = (edad >= 18) and tiene_permiso
+print("puedes salir")
+
+
 
 
 
 # TODO 2: Usa una expresión booleana con "or" para permitir salir si es fin de semana o tiene permiso
+# Por ejemplo: ¿Puede salir si es fin de semana O si tiene permiso?
+print("¿Puede salir si es fin de semana o si tiene permiso?")
+puede_salir_finde = es_finde or tiene_permiso
+print(puede_salir_finde)
 
 
 
 # TODO 3: Usa una expresión booleana con "not" para negar una condición
 # Por ejemplo: De ninguna manera tiene permiso
+print("¿Tiene permiso?")
+print(not tiene_permiso)    
 
 
 
 # TODO 5: Escribe tu propia condición con and, or o not e imprimelo a la pantalla.
 # Ejemplo: ¿Puede conducir si tiene 18 o más y tiene permiso? u cualquier otra idea. 
-
+print("¿Puede conducir si tiene 18 o más y tiene permiso?")
+puede_conducir = (edad >= 18) and tiene_permiso
 
 
 
--- modulo5/ejercicio3_comidas/comida.py
+++ modulo5/ejercicio3_comidas/comida.py
@@ -6,20 +6,36 @@ Al final, muestra un mensaje explicando de dónde es originaria esa comida.
 
 # TODO #1:
 # Imprime un mensaje de bienvenida al programa de comidas de Latinoamérica.
+print("Bienvenido al programa de comidas de Latinoamérica.")
 
 # TODO #2:
 # Muestra al usuario una lista de al menos 5 opciones de comidas para elegir.
 
+print("Opciones: tacos, gorditas, baho, pupusas, enchiladas")
+    
 # TODO #3:
 # Guarda lo que el usuario escribió en una variable llamada `comida`.
+comida = input("¿Qué comida quieres conocer? ")
 
 # TODO #4:
 # Convierte lo ingresado a minúsculas para asegurar la comparación correcta.
+comida = comida.lower()
 
 # TODO #5:
 # Usa una estructura if / elif / else para verificar la comida elegida.
 # Imprime un mensaje con el país de origen para cada comida.
-
+if comida == "tacos":
+    print("Los tacos son típicos de México.")
+elif comida == "gorditas":
+    print("Las gorditas son típicas de Guatemala.")
+elif comida == "baho":
+    print("El baho es típico de El Salvador.")
+elif comida == "pupusas":
+    print("Las pupusas son típicas de El Salvador.")
+elif comida == "enchiladas":
+    print("Las enchiladas son típicas de México.")
+else:
+    print("No tenemos información sobre esa comida.")
 
 ## Ejemplo de salida esperada:
 """
